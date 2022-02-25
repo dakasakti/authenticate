d7512110e562b7e8dd0a1c661f0ec18f9d4868c7
dakasakti <dakasakti.id@gmail.com>
2022-02-25 09:39:48 +0700

    improve dashboard admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,15 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __('Admin') }}
-        </h2>
-    </x-slot>
+@extends('layouts.dashboard')
 
-    <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+@section('content')
+<h1>Index</h1>
+@endsection
---
+++ resources/views/templates/sidebar.blade.php
@@ -0,0 +1,24 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link " href="{{ route('admin') }}">
+          <i class="bi bi-grid"></i>
+          <span>Dashboard</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-heading">Pages</li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{ route('user') }}">
+          <i class="bi bi-person"></i>
+          <span>User</span>
+        </a>
+      </li><!-- End Profile Page Nav -->
+      
+    </ul>
+
+  </aside><!-- End Sidebar-->
